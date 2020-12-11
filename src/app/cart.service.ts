97df2ade5c1171a1c40420303009c12f4703bdb6
Luc-Leveque <luc.leveque78@gmail.com>
2020-12-11 16:15:13 +0100

    5 files modified, 6 added

--- src/app/cart.service.ts
+++ src/app/cart.service.ts
@@ -1,8 +1,29 @@
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
 
-@Injectable()
+import { HttpClient } from "@angular/common/http";
+
+@Injectable({
+  providedIn: "root"
+})
 export class CartService {
+  items = [];
+  constructor(private http: HttpClient) {}
 
-  constructor() { }
+  addToCart(product) {
+    this.items.push(product);
+    console.log(this.items);
+  }
 
+  getItems() {
+    return this.items;
+  }
+
+  clearCart() {
+    this.items = [];
+    return this.items;
+  }
+
+  getShippingPrices() {
+    return this.http.get("/assets/shipping.json");
+  }
 }
